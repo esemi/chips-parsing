9744cc8e49654599a36cd7c1f9fd2f614fb75c91
Simon <spam@email.ru>
2017-11-04 12:59:44 +0300

    fix sloowly code gen (already parsed check) and use logging

--- parse.py
+++ parse.py
@@ -1,17 +1,23 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import random
 import time
 import math
 from string import ascii_uppercase
+
 from collections import deque, Counter
 from itertools import product
+import logging
 
 import asyncio
 import aiohttp
 import aiofiles
 
-# todo path
+# todo read about asyncio.shield
+# todo read about aiojobs
+# todo read about timeouts
+# todo files path
+
 SOURCE_PARSED_INVALID = 'data/parsed.txt'
 SOURCE_PARSED_VALID = 'data/valid.txt'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
@@ -31,9 +37,9 @@ CODE_PREFIX = ''
 CODE_CHARS = ascii_uppercase
 CODE_LEN = 10
 
-CODES_PER_SESSION_LIMIT = 30000   # limit per one parsing session
+CODES_PER_SESSION_LIMIT = 50000   # limit per one parsing session
 PROXY = None
-MAX_SIMULTANEOUSLY_REQUEST = 500  # limit per onetime
+MAX_SIMULTANEOUSLY_REQUEST = 5000  # limit per onetime
 CODES_BUFFER_LIMIT = 1000
 CODES_PER_TASK_MIN = 3   # three is limitation in browser,
 CODES_PER_TASK_MAX = 30  # but we can more
@@ -48,15 +54,12 @@ except ImportError:
 
 
 assert CODES_PER_TASK_MAX >= CODES_PER_TASK_MIN
-
-# todo compare speed bulk vs minimal validation request
+assert MAX_SIMULTANEOUSLY_REQUEST
 
 
 class Storage(object):
 
-    invalid_file = None
     invalid_asyncfile = None
-    valid_file = None
     valid_asyncfile = None
 
     codes_queue = deque()
@@ -69,33 +72,33 @@ class Storage(object):
     async def create(cls):
         instance = cls()
         await instance.init_fs()
-        instance.generate_codes()
+        instance.generate_codes(CODE_PREFIX)
         return instance
 
-    def generate_codes(self):
-        combination_len = CODE_LEN - len(CODE_PREFIX)
-        log('Generate code by len %s' % combination_len)
-        for comination in product(CODE_CHARS, repeat=combination_len):
-            code = '%s%s' % (CODE_PREFIX, ''.join(comination))
-            if not self.is_already_parsed_code(code):
-                self.codes_queue.append(code)
-
-            if len(self.codes_queue) >= CODES_PER_SESSION_LIMIT:
-                break
-
-        log('Gen %d codes' % len(self.codes_queue))
+    def generate_codes(self, prefix):
+        with open(SOURCE_PARSED_INVALID, 'r') as invalid_f, open(SOURCE_PARSED_VALID, 'r') as valid_f:
+            s1, s2 = invalid_f.read(), valid_f.read()
+            combination_len = CODE_LEN - len(prefix)
+            logging.info('Generate code by len %s' % combination_len)
+            chars = list(CODE_CHARS)
+            random.shuffle(chars)
+            for combination in product(''.join(chars), repeat=combination_len):
+                code = '%s%s' % (prefix, ''.join(combination))
+                logging.debug('check code %s', code)
+                if not self.is_already_parsed_code(code, s1, s2):
+                    self.codes_queue.append(code)
+                if len(self.codes_queue) >= CODES_PER_SESSION_LIMIT:
+                    break
+            logging.info('Gen %d codes' % len(self.codes_queue))
 
     async def init_fs(self):
         # use mmap if need more speed
         self.invalid_asyncfile = await aiofiles.open(SOURCE_PARSED_INVALID, 'a+')
-        self.invalid_file = open(SOURCE_PARSED_INVALID, 'r')
         self.valid_asyncfile = await aiofiles.open(SOURCE_PARSED_VALID, 'a+')
-        self.valid_file = open(SOURCE_PARSED_VALID, 'r')
 
-    def is_already_parsed_code(self, code):
-        self.invalid_file.seek(0)
-        self.valid_file.seek(0)
-        return self._prepare_code_for_save(code) in self.valid_file.read() or self._prepare_code_for_save(code) in self.invalid_file.read()
+    def is_already_parsed_code(self, code, invalid_source, valid_source):
+        return self._prepare_code_for_save(code) in valid_source or \
+               self._prepare_code_for_save(code) in invalid_source
 
     @staticmethod
     def _prepare_code_for_save(code):
@@ -116,31 +119,26 @@ class Storage(object):
     async def flush_buffers(self):
         # fixme dirty code
         if self.codes_buffer_valid:
-            log('flush buffer valid', True)
+            logging.info('flush buffer valid')
             await self.valid_asyncfile.write(
                 ''.join([self._prepare_code_for_save(code) for code in self.codes_buffer_valid]))
             self.stats['valid'] += len(self.codes_buffer_valid)
             self.codes_buffer_valid = []
 
         if self.codes_buffer_invalid:
-            log('flush buffer invalid', True)
+            logging.info('flush buffer invalid')
             await self.invalid_asyncfile.write(
                 ''.join([self._prepare_code_for_save(code) for code in self.codes_buffer_invalid]))
             self.stats['invalid'] += len(self.codes_buffer_invalid)
             self.codes_buffer_invalid = []
 
 
-def log(msg, force=False):
-    # todo use logging
-    if DEBUG or force:
-        print(msg)
-
 async def parsing(session, codes, pid, storage):
     start = time.time()
     async with session.post(URL, data={'codes': codes}, headers={'User-Agent': USER_AGENT, 'Referer': REFERER},
                             proxy=PROXY) as resp:
         response_json = await resp.json()
-        log('Process {}: {} {}, took: {:.2f} seconds'.format(
+        logging.debug('Process {}: {} {}, took: {:.2f} seconds'.format(
             pid, codes, response_json, time.time() - start))
 
         for i, r in enumerate(response_json['result']):
@@ -148,19 +146,19 @@ async def parsing(session, codes, pid, storage):
                 continue
 
             is_valid = r['status'] == VALID_RESPONSE
-            log('save code %s %s' % (codes[i], int(is_valid)))
+            logging.debug('save code %s %s' % (codes[i], int(is_valid)))
             await storage.save_parsed_code(codes[i], is_valid)
 
             if is_valid:
-                log('WINNER %s' % codes[i], True)
+                logging.warning('success code %s' % codes[i])
 
             if r['status'] not in {VALID_RESPONSE, INVALID_RESPONSE, USED_RESPONSE}:
-                log('UNKNOWN STATUS %s' % r['status'], True)
+                logging.error('UNKNOWN STATUS %s' % r['status'])
 
 
 async def task(pid, storage: Storage, sem: asyncio.Semaphore):
     async with sem:
-        log('Task {} started'.format(pid))
+        logging.debug('Task {} started'.format(pid))
         codes = []
         for i in range(CODES_PER_TASK_MAX):
             try:
@@ -169,20 +167,20 @@ async def task(pid, storage: Storage, sem: asyncio.Semaphore):
                 pass
 
         if len(codes) < CODES_PER_TASK_MIN:
-            log('skip task by not found codes %s' % codes, True)
+            logging.info('skip task by not found codes %s' % codes)
             return
 
         async with aiohttp.ClientSession() as session:
             try:
                 await parsing(session, codes, pid, storage)
             except aiohttp.client_exceptions.ClientError as e:
-                log('Exception %s' % e, True)
+                logging.warning('Exception %s' % e)
 
 async def run():
     storage = await Storage.create()
     sem = asyncio.Semaphore(MAX_CLIENTS)
     task_count = math.ceil(len(storage.codes_queue) / CODES_PER_TASK_MAX)
-    log('Create %d tasks' % task_count, True)
+    logging.info('Create %d tasks' % task_count)
     if not task_count:
         return
 
@@ -193,13 +191,16 @@ async def run():
     await storage.flush_buffers()
     end_time_in_sec = time.time() - start_time
 
-    log("Process took: %.2f seconds (%.2f req/sec)" % (end_time_in_sec,
-                                                       sum(storage.stats.values()) / end_time_in_sec), True)
-    log('Result: invalid codes %d; valid codes %d' % (storage.stats['invalid'], storage.stats['valid']), True)
+    logging.info("Process took: %.2f seconds (%.2f req/sec)" % (end_time_in_sec,
+                                                                sum(storage.stats.values()) / end_time_in_sec))
+    logging.info('Result: invalid codes %d; valid codes %d' % (storage.stats['invalid'], storage.stats['valid']))
 
 
 if __name__ == '__main__':
-    log('Start %s clients per %s codes max:' % (MAX_CLIENTS, CODES_PER_TASK_MAX), True)
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)s:%(message)s',
+        level=logging.DEBUG if DEBUG else logging.INFO)
+    logging.info('Start %s clients per %s codes max:' % (MAX_CLIENTS, CODES_PER_TASK_MAX))
     ioloop = asyncio.get_event_loop()
     ioloop.run_until_complete(run())
     ioloop.close()
